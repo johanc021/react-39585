1c5db8b28215fec899dd81e393aa51cc3cf45963
johan021 <facjohan@hotmail.com>
2023-03-11 20:55:27 -0500

    Algunas mejoras :)

--- src/components/RatingBook/RatingBook.js
+++ src/components/RatingBook/RatingBook.js
@@ -3,19 +3,19 @@ import React from 'react';
 
 const RatingBook = ({rating}) => {
 
-	const filledStars = Math.floor(Number(rating) );
-	const hasHalfStar = rating - filledStars >= 0.5;
-	const emptyStars = 5 - filledStars - (hasHalfStar ? 1 : 0);
+	const estrellasCompletas = Math.floor(Number(rating) );
+	const estrellasMitad = rating - estrellasCompletas >= 0.5;
+	const estrellasVacias = 5 - estrellasCompletas - (estrellasMitad ? 1 : 0);
 
 	return (
 		<div className="flex items-center">
 			<Rating size="lg">
 
-				{[...Array(filledStars)].map((_, index) => (
+				{[...Array(estrellasCompletas)].map((_, index) => (
 					<Rating.Star key={`filled-${index}`} />
 				))}
-				{hasHalfStar && <Rating.Star half key="half-filled" />}
-				{[...Array(emptyStars)].map((_, index) => (
+				{estrellasMitad && <Rating.Star half key="half-filled" />}
+				{[...Array(estrellasVacias)].map((_, index) => (
 					<Rating.Star filled={false} key={`empty-${index}`} />
 				))}
 				<p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
